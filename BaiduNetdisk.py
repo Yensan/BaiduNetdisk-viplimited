3284506413361cefeafc6a3e6f77f0478021e373
Yensan <yensan@live.cn>
2019-01-10 22:23:05 +0800

    Update BaiduNetdisk.py

--- BaiduNetdisk.py
+++ BaiduNetdisk.py
@@ -1,64 +1,74 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import subprocess
-import os, stat
+import os, stat, platform
 import datetime, time
 import sqlite3
 
+plat = platform.platform()[0]
+
 def is_BaiduNetdisk_running():
-    out = subprocess.getoutput('ps aux | grep BaiduNetdisk_mac | grep -v grep')
-    return True if out else False
+    cmd = {'D': 'ps aux |grep BaiduNetdisk_mac |grep -v grep',
+           'W': 'tasklist | findstr BaiduNetdisk.exe'}
+    return True if subprocess.getoutput(cmd[plat]) else False
 
 def getDIR():
-    path = os.path.abspath(os.path.expanduser('~') +
-        "/Library/Application Support/com.baidu.BaiduNetdisk-mac")
-    out = subprocess.getoutput("ls -t '{}'".format(path))
-    dirs = [i for i in out.split('\n') if len(i) > 12]
-    if dirs: return os.path.join(path, dirs[0])
-    else: raise RuntimeError("不能取得 BaiduNetdisk 数据")
+    BaiduPath = {'D': "Library/Application Support/com.baidu.BaiduNetdisk-mac",
+                 'W': r'AppData\Roaming\baidu\BaiduNetdisk\users'}
+    path = os.path.join(os.path.expanduser('~'), BaiduPath[plat])
+    dirs = [i for i in os.listdir(path) if len(i) > 12]
+    if dirs:
+        return os.path.join(path, dirs[0])
+    else:
+        raise RuntimeError("不能取得 BaiduNetdisk 数据文件夹。您的百度盘安装路径很独特")
 
-def getFiles():
-    dbfile = os.path.join(getDIR(),'transmission.db')
+def getFiles(dbpath):
     limit = datetime.datetime.now() - datetime.timedelta(minutes=5)
-    limit = time.mktime(limit.timetuple())
-    with sqlite3.connect(dbfile) as db:
+    limit = int(time.mktime(limit.timetuple()))
+    # 'SELECT * FROM downloads ORDER BY fid DESC LIMIT 1;'
+    sql = {'D': 'SELECT local_path, local_name FROM downloads WHERE task_update_time > {} AND file_name NOT null;',
+           'W': 'SELECT local_path FROM download_file WHERE status_changetime > {} AND local_path NOT null;'}
+    files = None
+    with sqlite3.connect(dbpath) as db:
         cursor = db.cursor()
-        rows = cursor.execute('SELECT * FROM downloads WHERE task_update_time > {} AND file_name NOT null;'.format(limit))
-        files = []  # 'SELECT * FROM downloads ORDER BY fid DESC LIMIT 1;'
-        for row in rows:
-            d = {col[0]:row[idx] for idx, col in enumerate(cursor.description)}
-            files.append(os.path.join(d['local_path'], d['local_name']))
-        return files
+        rows = cursor.execute(sql[plat].format(limit))
+        if plat == 'D':
+            files = [os.path.join(row[0], row[1]) for row in rows]
+        elif plat == 'W':
+            files = [os.path.join(row[0]) for row in rows]
+    return files
 
-def rm_rf(path):
+def rm_dbfile(path, file):
     for fileList in os.walk(path):
-        files = [f for f in fileList[2] if 'transmission' in f]
-        # files = fileList[2]  这样会删除所有的文件，没有必要
+        files = [f for f in fileList[2] if file in f]
         for name in files:
             os.chmod(os.path.join(fileList[0], name), stat.S_IWRITE)
             os.remove(os.path.join(fileList[0], name))
 
 def main():
     echos = ["百度盘高速试用完毕，退出百度盘，运行这个脚本；",
-            "再次打开百度盘，重新下载这个文件，然后立即退出百度盘；",
-            "再次打开百度盘，你就又可以再次高速下载啦！"]
-    for i, s in enumerate(echos): print('{}. {}'.format(i+1, s))
+             "再次打开百度盘，重新下载这个文件，然后立即退出百度盘；",
+             "再次打开百度盘，你就又可以再次高速下载啦！"]
+    for i, s in enumerate(echos): print('{}. {}'.format(i + 1, s))
+    BdNdisk_path = getDIR()
+    extension = {'D': '.bdc-downloading', 'W': ''}
+    dbfiles = {'D': 'transmission.db', 'W': 'BaiduYunGuanjia.db'}
     ori_files, bak_files = None, None
     while 1:
         if not is_BaiduNetdisk_running():
             # 1. 退出百度盘后，把文件改名（备份）
-            # ori_files = getFiles()
-            ori_files = [i + '.bdc-downloading' for i in getFiles()]
-            bak_files = [i + '.bak' for i in ori_files]
+            dbpath = os.path.join(BdNdisk_path, dbfiles[plat])
+            downloadFiles = getFiles(dbpath)
+            ori_files = [i + extension[plat] for i in downloadFiles]
+            bak_files = [i + '.bak' for i in downloadFiles]
             for ori, bak in zip(ori_files, bak_files):
                 os.rename(ori, bak)
             # 2. 删除百度盘下载数据
-            rm_rf(getDIR())
+            rm_dbfile(BdNdisk_path, dbfiles[plat])
             break
     # 3. 在百度盘退出、重启前，把备份的文件恢复
     while 1:
-        if is_BaiduNetdisk_running():
-            break
+        if is_BaiduNetdisk_running(): break
     while 1:
         if not is_BaiduNetdisk_running():
             try:
@@ -68,7 +78,6 @@ def main():
             except FileNotFoundError as e:
                 print(e, '\n', '重新下载这个文件，之后，再退出百度盘')
             break
-
 
 if __name__ == "__main__":
     main()
